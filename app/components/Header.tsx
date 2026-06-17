921994c7b3a57ee091fd7116d02ec0be1a67cac6
Eshan Gupta <eshan07gupta@gmail.com>
2026-06-17 16:42:40 +0100

    Fixed an issue where live page downloaded incorrect CV

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -29,7 +29,7 @@ export default function Header() {
           <Image src={assets.right_arrow_white} alt="" className="w-4" />
         </a>
         <a
-          href="/Eshan-Gupta.pdf"
+          href="./Eshan-Gupta.pdf"
           download
           className="px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black"
         >
